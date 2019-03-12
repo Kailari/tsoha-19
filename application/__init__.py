2a3af43b9c3e9cbcf7d09f7d66f1ebcf82d78bcd
toilari <ilari97@hotmail.com>
2019-03-12 13:50:36 +0200

    Added support for Heroku DB

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -12,4 +17,7 @@ from application import views
 from application.posts import models
 from application.posts import views
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
