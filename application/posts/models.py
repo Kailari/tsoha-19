6e2b4a242f6f50a08cc4f07cc725c274efba0ede
toilari <ilari97@hotmail.com>
2019-03-13 00:28:02 +0200

    Cleaned up some leftovers from timestamp processing

--- application/posts/models.py
+++ application/posts/models.py
@@ -3,13 +3,6 @@ from sqlalchemy import text
 from application.models import WithIDAndDatesCreatedAndModified
 import os
 
-if os.environ.get("HEROKU"):
-    get_all_stmt = text()
-else:
-    get_all_stmt = text("SELECT"
-                        "")
-
-
 class Post(WithIDAndDatesCreatedAndModified):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
