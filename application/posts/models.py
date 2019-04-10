83ceb157f64947d4f50cb51f215d56bf91bd2083
toilari <ilari97@hotmail.com>
2019-04-10 22:42:27 +0300

    Made walls use improved query too

--- application/posts/models.py
+++ application/posts/models.py
@@ -28,23 +28,24 @@ class Post(WithIDAndDatesCreatedAndModified):
     @staticmethod
     def get_posts_for_user_wall(user_id, older_than, limit=-1):
         stmt = text("SELECT "
-                    " Post.id AS post_id,"
-                    " Post.owner_id AS poster_id,"
-                    " Poster.name AS poster_name,"
-                    " WallOwner.id AS wall_owner_id,"
-                    " WallOwner.name AS wall_owner_name,"
-                    " Post.date_created AS date_created,"
-                    " Post.date_modified AS date_modified,"
-                    " Post.content AS content,"
-                    " COUNT(Comment.id) AS comment_count"
+                    "  Post.id AS post_id"
+                    ", PostedBy.id AS poster_id"
+                    ", PostedBy.name AS poster_name"
+                    ", WallOwner.id AS wall_owner_id"
+                    ", WallOwner.name AS wall_owner_name"
+                    ", Post.date_created AS date_created"
+                    ", Post.date_modified AS date_modified"
+                    ", Post.content AS content"
+                    ", (SELECT COUNT(Comment.id) AS comment_count"
+                    "   FROM Comment "
+                    "   WHERE Comment.post_id = Post.id"
+                    "  )"
                     " FROM Post"
-                    "  LEFT JOIN Comment ON Comment.post_id = Post.id"
-                    "  INNER JOIN Account AS Poster ON Poster.id = Post.owner_id"
                     "  INNER JOIN Wall ON Wall.id = Post.wall_id"
+                    "  INNER JOIN Account AS PostedBy ON Post.owner_id = PostedBy.id"
                     "  INNER JOIN Account AS WallOwner ON WallOwner.id = Wall.id"
                     " WHERE WallOwner.id = :user_id"
                     "   AND Post.date_created < CAST(:older_than AS timestamp)"
-                    "  GROUP BY Post.id, Poster.id, Poster.name, WallOwner.id"
                     "  ORDER BY Post.date_created DESC" +
                     ("  LIMIT :limit" if limit > 0 else "")
                     ).params(user_id=user_id, older_than=str(older_than), limit=limit)
@@ -52,17 +53,7 @@ class Post(WithIDAndDatesCreatedAndModified):
 
         posts = []
         for row in res:
-            posts.append({
-                "post_id": row["post_id"],
-                "poster_id": row["poster_id"],
-                "poster_name": row["poster_name"],
-                "wall_owner_id": row["wall_owner_id"],
-                "wall_owner_name": row["wall_owner_name"],
-                "date_created": row["date_created"],
-                "date_modified": row["date_modified"],
-                "content": row["content"],
-                "comment_count": row["comment_count"],
-            })
+            posts.append(row)
         return posts
 
     @staticmethod
