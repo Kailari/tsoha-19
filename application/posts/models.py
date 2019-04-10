bdb61eb31d7cf7b2bd027687995bd07469b72e49
toilari <ilari97@hotmail.com>
2019-04-10 22:23:36 +0300

    Cleaning up the way results are handled

--- application/posts/models.py
+++ application/posts/models.py
@@ -69,8 +69,8 @@ class Post(WithIDAndDatesCreatedAndModified):
     def get_posts_for_user_feed(user_id, older_than, limit=-1):
         stmt = text("SELECT "
                     "  Post.id AS post_id"
-                    ", PostedBy.id AS postedby_id"
-                    ", PostedBy.name AS postedby_name"
+                    ", PostedBy.id AS poster_id"
+                    ", PostedBy.name AS poster_name"
                     ", WallOwner.id AS wall_owner_id"
                     ", WallOwner.name AS wall_owner_name"
                     ", Post.date_created AS date_created"
@@ -84,14 +84,12 @@ class Post(WithIDAndDatesCreatedAndModified):
                     "  INNER JOIN Wall ON Wall.id = Post.wall_id"
                     "  INNER JOIN Account AS PostedBy ON Post.owner_id = PostedBy.id"
                     "  INNER JOIN Account AS WallOwner ON WallOwner.id = Wall.id"
-                    # Owner check is needed to prevent other users' subscriptions from 
-                    # creating duplicate lines in the results. Avoids having to add 
-                    # a GROUP BY clause. Needs to be a left join to keep posts received
+                    # Owner check is needed to prevent other users' subscriptions from
+                    # creating duplicate lines in the results. Avoids having to add
+                    # a GROUP BY clause. Needs to be a LEFT JOIN to keep posts received
                     # without a subscription (e.g. posts from user itself) in the results.
                     "  LEFT JOIN Subscription ON Subscription.owner_id = :user_id"
                     "                        AND Subscription.wall_id = Wall.id"
-                    # TODO: Should users automagically have a subscription on themselves?
-                    #       Would avoid special-casing the owner here.
                     "  WHERE (Subscription.owner_id = :user_id OR Post.owner_id = :user_id OR Post.wall_id = :user_wall_id)"
                     "    AND Post.date_created < CAST(:older_than AS timestamp)"
                     "  ORDER BY Post.date_created DESC" +
@@ -101,15 +99,6 @@ class Post(WithIDAndDatesCreatedAndModified):
 
         posts = []
         for row in res:
-            posts.append({
-                "post_id": row["post_id"],
-                "poster_id": row["postedby_id"],
-                "poster_name": row["postedby_name"],
-                "wall_owner_id": row["wall_owner_id"],
-                "wall_owner_name": row["wall_owner_name"],
-                "date_created": row["date_created"],
-                "date_modified": row["date_modified"],
-                "content": row["content"],
-                "comment_count": row["comment_count"],
-            })
+            posts.append(row)
+
         return posts
